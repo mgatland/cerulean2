bf4573612e4a2a32c5ef5538bb9c46ed0a6d6290
mgatland <mgatland@gmail>
2013-08-08 15:53:55 +1200

    User can move a square around the screen

---
+++ js/cerulean.js
@@ -0,0 +1,65 @@
+var Cerulean = function () {
+
+	var GameWindow = function () {
+		this.width = 1024;
+		this.height = 768;
+	}
+
+	var Renderer = function (gameWindow) {
+		var canvas;
+		var ctx;
+		
+		canvas = document.getElementById('gamescreen');
+		ctx = canvas.getContext("2d");
+		canvas.width = gameWindow.width;
+		canvas.height = gameWindow.height;
+
+		this.draw = function (playerPos) {
+			ctx.fillStyle = "#3f303f";
+			ctx.fillRect(0,0, gameWindow.width, gameWindow.height);
+			ctx.fillStyle = "white";
+			ctx.font = '32px Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif';
+			ctx.fillText("Cerulean 2…", 40, gameWindow.height - 32);
+			ctx.fillRect(playerPos.x, playerPos.y, 32, 32);
+		}
+	}
+
+	this.start = function () {
+		var gameWindow = new GameWindow();
+		var renderer = new Renderer(gameWindow);
+		var keyboard = new Keyboard();
+		var playerPos = new Pos(0,0);
+
+		var update = function () {
+			if (keyboard.isKeyDown(KeyEvent.DOM_VK_RIGHT)) {
+				playerPos.x++;
+			} else if (keyboard.isKeyDown(KeyEvent.DOM_VK_LEFT)) {
+				playerPos.x--;
+			}
+			if (keyboard.isKeyDown(KeyEvent.DOM_VK_UP)) {
+				playerPos.y--;
+			} else if (keyboard.isKeyDown(KeyEvent.DOM_VK_DOWN)) {
+				playerPos.y++;
+			}
+			keyboard.update();
+		}
+
+		//cross browser hacks
+		var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+		window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  		window.requestAnimationFrame = requestAnimationFrame;
+
+		window.setInterval(function () {
+			update();
+			requestAnimationFrame(function() {
+				renderer.draw(playerPos);
+			});
+		});
+	}
+
+}
+
+window.onload = function () {
+	var cerulean = new Cerulean();
+	cerulean.start();
+};
---
+++ js/util.js
@@ -0,0 +1,10 @@
+var Pos = function (x, y) {
+	this.x = x;
+	this.y = y;
+
+	this.toString = function () {
+		return "(" + this.x + "," + this.y + ")";
+	}
+
+
+}
