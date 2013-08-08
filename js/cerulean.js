78d4cf93843d740ddd7670acad067a6b3ecaec61
mgatland <mgatland@gmail>
2013-08-08 16:22:21 +1200

    add room concept

--- js/cerulean.js
+++ js/cerulean.js
@@ -5,6 +5,12 @@ var Cerulean = function () {
 		this.height = 768;
 	}
 
+	var GameConsts = {
+		tileSize: 32,
+		worldWidth: 300,
+		worldHeight: 300
+	};
+
 	var Renderer = function (gameWindow) {
 		var canvas;
 		var ctx;
@@ -14,14 +20,25 @@ var Cerulean = function () {
 		canvas.width = gameWindow.width;
 		canvas.height = gameWindow.height;
 
-		this.draw = function (playerPos) {
+		this.draw = function (playerPos, room) {
 			ctx.fillStyle = "#3f303f";
 			ctx.fillRect(0,0, gameWindow.width, gameWindow.height);
 			ctx.fillStyle = "white";
 			ctx.font = '32px Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif';
 			ctx.fillText("Cerulean 2…", 40, gameWindow.height - 32);
+
+			ctx.fillStyle = "#bbffbb";
+			ctx.fillRect(room.pos.x*GameConsts.tileSize, room.pos.y*GameConsts.tileSize, 
+				room.size.x*GameConsts.tileSize, room.size.y*GameConsts.tileSize);
+
+			ctx.fillStyle = "white";
 			ctx.fillRect(playerPos.x, playerPos.y, 32, 32);
 		}
+	}
+
+	var Room = function (x, y, width, height) {
+		this.pos = new Pos(x, y);
+		this.size = new Pos(width, height);
 	}
 
 	this.start = function () {
@@ -30,6 +47,7 @@ var Cerulean = function () {
 		var keyboard = new Keyboard();
 		var desiredFps = 60;
 
+		var room = new Room(2,2,5,5);
 		var playerPos = new Pos(0,0);
 
 		var update = function () {
